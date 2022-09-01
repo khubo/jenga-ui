ec3b0f09293dc95289288e605b52a2868d988ee8
shubham-kaushal <nsk9195@gmail.com>
2022-09-01 17:10:55 +0530

    update config and deps.

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -2,17 +2,15 @@ import type { NextPage } from 'next'
 
 import { Avatar } from '@jenga-ui/avatar'
 import {Block} from '@jenga-ui/core'
-import { Accordion } from '@jenga-ui/accordion'
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div >
-
       <h1> hello</h1>
       <Block> and it is this</Block>
-      <Avatar> asdf</Avatar>
-      <Accordion><span> hi</span></Accordion>
+      <Avatar icon={null}>OP</Avatar>
     </div>
   )
 }
